02ea83f9da5ea0253e6aab7d5c2647c6d6b2ada6
heenakhan3133 <36080812+heenakhan3133@users.noreply.github.com>
2020-12-01 23:16:49 +0530

    object creation

--- Program.cs
+++ Program.cs
@@ -1,16 +1,30 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace BasicClassConcept
+namespace objCalling
 {
     class Program
     {
         static void Main(string[] args)
         {
+            class1 o;
+            o = new class1();
+            o.display();
+            Console.ReadLine();
 
+        }
+    }
 
-           System.Console.WriteLine("Hello World");
-           System.Console.ReadLine();
+    public class class1
+    {
+        public void display()
+        {
+            Console.WriteLine("Hello world");
         }
     }
 }
+
+
